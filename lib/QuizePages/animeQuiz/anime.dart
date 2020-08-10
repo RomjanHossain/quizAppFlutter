6b5b5dc47c4c76528433ff7f23dd9c918e94ac38
RomjanHossain <romjanhossain726526@gmail.com>
2020-08-10 23:02:37 +0600

    created animePage folder

---
+++ lib/QuizePages/animeQuiz/anime.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'animeQuestion.dart';
+
+AnimeQuestion myQuestions = AnimeQuestion();
+
+class AnimePage extends StatefulWidget {
+  AnimePage({Key key}) : super(key: key);
+
+  @override
+  _AnimePageState createState() => _AnimePageState();
+}
+
+class _AnimePageState extends State<AnimePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('Anime'),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/anime/bg1.jpg'),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '${myQuestions.index()}: ${myQuestions.getQuestion()}',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.transparent,
+              width: MediaQuery.of(context).size.width / 1.1,
+              height: MediaQuery.of(context).size.height / 1.9,
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        color: Colors.lightBlueAccent,
+                        child: ListTile(
+                          leading: Text(
+                            'A',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          title: Text(
+                            'Anime',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          onTap: () {},
+                          onLongPress: () {
+                            setState(() {
+                              myQuestions.nextQuestion();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        color: Colors.lightBlueAccent,
+                        child: ListTile(
+                          leading: Text(
+                            'B',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          title: Text(
+                            'Chemistry',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        color: Colors.lightBlueAccent,
+                        child: ListTile(
+                          leading: Text(
+                            'C',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          title: Text(
+                            'General Knowledge',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        color: Colors.lightBlueAccent,
+                        child: ListTile(
+                          leading: Text(
+                            'D',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          title: Text(
+                            'General Knowledge',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/QuizePages/animeQuiz/animeQuestion.dart
@@ -0,0 +1,56 @@
+import '../question.dart';
+
+class AnimeQuestion {
+  int count = 0;
+  List<Question> _questions = [
+    Question(
+        'Clefairy was intended to be Ash’s starting Pokémon in the pilot episode of the cartoon',
+        3), // true
+    Question(
+        "What year was “JoJo’s Bizarre Adventure: Phantom Blood” first released?",
+        2), //1987
+    Question(
+        'In “One Piece”, what does “the Pirate King” mean to the captain of the Straw Hat Pirates?',
+        3), //freedom
+    Question('Who created the animated movie “Spirited Away” (2001)?', 3),
+    Question(
+        'What caused the titular mascot of Yo-Kai Watch, Jibanyan, to become a yokai?',
+        2),
+    Question(
+        'Who is the main character with yellow hair in the anime Naruto?', 2),
+    Question(
+        'The anime Attack on Titan was directed by Tetsurō Araki, the same person who directed the anime Highschool of the Dead',
+        2),
+    Question(
+        'Throughout the entirety of “Dragon Ball Z”, Goku only kills two characters: a miniboss named Yakon and Kid Buu',
+        2),
+    Question('What is the name of the corgi in Cowboy Bebop?', 2),
+    Question(
+        'In Dragon Ball Z, who was the first character to go Super Saiyan 2?',
+        2),
+    Question(
+        'What is the name of the main character of the anime “One-Punch Man”?',
+        2),
+    Question(
+        'The name of Junko Enoshima’s imposter at the beginning of Danganronpa: Trigger Happy Havoc is?',
+        2),
+    Question(
+        'In the 2012 animated film “Wolf Children”, what are the names of the wolf children?',
+        2),
+    Question('Which of these anime have over 7,500 episodes?', 2),
+    Question('What year did “Attack on Titan” Season 2 begin airing?', 2),
+  ];
+  void nextQuestion() {
+    if (count < _questions.length) {
+      count++;
+    }
+  }
+
+  String getQuestion() {
+    return _questions[count].q;
+  }
+
+  int index() {
+    return count + 1;
+  }
+}
