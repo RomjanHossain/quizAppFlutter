82bb449dd022cd08c6446e575be4d2513c09f9f4
RomjanHossain <romjanhossain726526@gmail.com>
2020-08-10 23:09:30 +0600

    quick update

--- lib/QuizePages/animeQuiz/animeQuestion.dart
+++ lib/QuizePages/animeQuiz/animeQuestion.dart
@@ -12,33 +12,35 @@ class AnimeQuestion {
     Question(
         'In “One Piece”, what does “the Pirate King” mean to the captain of the Straw Hat Pirates?',
         3), //freedom
-    Question('Who created the animated movie “Spirited Away” (2001)?', 3),
+    Question('Who created the animated movie “Spirited Away” (2001)?',
+        3), //Hayao Miyazaki
     Question(
         'What caused the titular mascot of Yo-Kai Watch, Jibanyan, to become a yokai?',
-        2),
+        2), //Being run over by a truck
     Question(
         'Who is the main character with yellow hair in the anime Naruto?', 2),
     Question(
         'The anime Attack on Titan was directed by Tetsurō Araki, the same person who directed the anime Highschool of the Dead',
-        2),
+        2), //true
     Question(
         'Throughout the entirety of “Dragon Ball Z”, Goku only kills two characters: a miniboss named Yakon and Kid Buu',
-        2),
-    Question('What is the name of the corgi in Cowboy Bebop?', 2),
+        2), // kid Buu
+    Question('What is the name of the corgi in Cowboy Bebop?', 2), // Einstein
     Question(
         'In Dragon Ball Z, who was the first character to go Super Saiyan 2?',
-        2),
+        2), // Gohan
     Question(
         'What is the name of the main character of the anime “One-Punch Man”?',
-        2),
+        2), //Saitama
     Question(
         'The name of Junko Enoshima’s imposter at the beginning of Danganronpa: Trigger Happy Havoc is?',
-        2),
+        2), // Mukuro Ikusaba
     Question(
         'In the 2012 animated film “Wolf Children”, what are the names of the wolf children?',
-        2),
-    Question('Which of these anime have over 7,500 episodes?', 2),
-    Question('What year did “Attack on Titan” Season 2 begin airing?', 2),
+        2), //Ame and Yuki
+    Question('Which of these anime have over 7,500 episodes?', 2), //Sazae-san
+    Question(
+        'What year did “Attack on Titan” Season 2 begin airing?', 2), //2017
   ];
   void nextQuestion() {
     if (count < _questions.length) {
